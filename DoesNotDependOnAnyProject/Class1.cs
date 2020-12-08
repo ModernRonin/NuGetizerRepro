a987f9e1581984d3809de4bc8887ffade102de0c
Mark Hajek <markhajek@outlook.com>
2020-12-08 10:33:34 +0000

    added projects

---
+++ DoesNotDependOnAnyProject/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DoesNotDependOnAnyProject
+{
+    public class Class1
+    {
+    }
+}
---
+++ GetsPublishedAsNuget/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GetsPublishedAsNuget
+{
+    public class Class1
+    {
+    }
+}
